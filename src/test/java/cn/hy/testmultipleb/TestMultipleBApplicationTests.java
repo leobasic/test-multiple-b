fad92870f6875467e179f5949b8bf5129cc0c469
zhaoxiangxiong <zhaoxiangxiong@haoyuntech.com>
2020-08-25 10:27:06 +0800

    b

---
+++ src/test/java/cn/hy/testmultipleb/TestMultipleBApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.hy.testmultipleb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestMultipleBApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
