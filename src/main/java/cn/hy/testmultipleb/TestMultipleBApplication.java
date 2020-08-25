7da7973977a0929d7962f50d96bd49495c370b1e
zhaoxiangxiong <zhaoxiangxiong@haoyuntech.com>
2020-08-25 12:17:30 +0800

    b项目提交修改

--- src/main/java/cn/hy/testmultipleb/TestMultipleBApplication.java
+++ src/main/java/cn/hy/testmultipleb/TestMultipleBApplication.java
@@ -3,6 +3,9 @@ package cn.hy.testmultipleb;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 修改测试2
+ */
 @SpringBootApplication
 public class TestMultipleBApplication {
 
